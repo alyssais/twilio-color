8adc8dce94c4e58f2113172642a9c656d59bffc8
Alyssa Ross <hi@alyssa.is>
2013-12-16 01:49:54 +0000

    Much improve

--- public/client.js
+++ public/client.js
@@ -1,8 +1,24 @@
 // connect to the websocket
 var socket = io.connect();
 
+var validColor = function (color) {
+  var elem = document.createElement("div");
+  elem.style.color = color;
+  return !!elem.style.color;
+};
+
+var showExample = function() {
+  document.getElementById("example").innerHTML = randomColor();
+};
+
+setInterval(showExample, 5000);
+showExample();
+
 // listen for new colors from the server
 socket.on('color', function(color) {
-	// update the background color
-  document.body.style.backgroundColor = color;
+  console.log(color);
+
+  document.body.style.backgroundColor = validColor(color) ? color : randomColor();
 });
+
+socket.emit('ready');
--- server.js
+++ server.js
@@ -1,37 +1,46 @@
 // load dependencies
 var twilio = require("twilio");
 var express = require("express.io");
+var shared = require("./public/shared");
+
+var authToken = require("fs").readFileSync("authtoken.txt").toString().trim();
 
 // create the express.io app
 var app = express();
 app.http().io();
+
+var lastColor;
 
 // send client code from the public/ folder
 app.use(express.static(process.cwd() + '/public'));
 
 // handle SMS from Twilio
 app.get('/twilio/sms', function(req, res) {
+  if (!twilio.validateExpressRequest(req, authToken)) {
+    console.log("We got a baddy");
+    res.send("Nice try.");
+    return;
+  }
+
   // extract the color from the Twilio request
-  var color = req.query.Body.trim();
+  var color = req.query.Body.toLowerCase().replace(/\s/g, "");
 
   console.log(color);
 
   // prepare the reply to Twilio
   var resp = new twilio.TwimlResponse();
-
-  // check that the color is valid
-  if (color.match(/^\#(?:[0-9a-f]{3}){1,2}$/i)) {
-    // broadcast the color to all connected browsers
-    app.io.broadcast('color', color);
-  } else {
-    // reply to the SMS, telling the sender that the color is invalid.
-    // generate a random example color to show what color format is expected.
-    var exampleColor = Math.floor(Math.random() * Math.pow(255, 3)).toString(16);
-    resp.message('I didn\'t recognize that color. :(\nTry sending a hex color, like this: "#' + exampleColor + '".');
-  }
+  
+  // broadcast the color to all connected browsers
+  app.io.broadcast('color', color);
+  lastColor = color;
 
   // send the reply to Twilio
   res.send(resp.toString());
+});
+
+app.io.route('ready', function(req) {
+  if (lastColor)
+    req.io.emit('color', lastColor);
 });
 
 app.listen(7076);
